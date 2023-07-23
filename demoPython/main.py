f09630f93a9094560b2cc7c598b245230a737638
Amit-Fexle-Training <122790801+Amit-Fexle-Training@users.noreply.github.com>
2023-07-23 17:05:50 +0530

    Create main.py

---
+++ demoPython/main.py
@@ -0,0 +1,29 @@
+import os
+from simple_salesforce import Salesforce, format_soql
+from pprint import pprint
+from datetime import datetime
+import configparser
+import warnings
+import time
+import pandas as pd
+import json
+
+OrgCreds = configparser.ConfigParser()
+OrgCreds.read('config.ini')
+
+username = OrgCreds.get('my_credentials', 'username')
+password = OrgCreds.get('my_credentials', 'password')
+security_token = OrgCreds.get('my_credentials', 'security_token')
+domain = OrgCreds.get('my_credentials', 'domain')
+
+# Establish a connection to your Salesforce instance
+print('Establish a connection to your Salesforce instance')
+source_sf = Salesforce(username=username, password=password, security_token= security_token, domain= domain)
+print('Connection Established')
+
+result = sf.query_all("SELECT Id, Name, Email FROM Contact")
+print(pd.DataFrame(result))
+records = result['records']
+for record in records:
+    print(f"Id: {record['Id']}, Name: {record['Name']}, Email: {record['Email']}")
+
